8fea739e0a72da936061dfed87ba31e06630625a
María Luena Rodríguez <maluenarod@gmail.com>
2018-05-14 17:21:35 +0200

    refactoring

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-
+function application(){
     'use strict';
 
 // Array Questions
@@ -48,63 +48,70 @@ var questions = [
 
 // Global variables
 
-let originalQuestions = questions;
-let score = 0;
-let time;
-let btnStartGame = document.getElementById('start__button');
-let btnNextQuestion = document.querySelector('.next__button');
-let btnNewGame = document.querySelector('.startNewGame');
-
+var originalQuestions = questions;
+var score = 0;
+var time;
+var btnStartGame = document.getElementById('start__button');
+var btnNextQuestion = document.querySelector('.next__button');
+let timeCounter;
 //Function to start the game
 
 function onStart(){
-    let dataSection = document.querySelector('.data__container');
-    btnStartGame.classList.add('invisible');
+    var dataSection = document.querySelector('.data__container');
+  //  btnStartGame.classList.add('invisible');
     dataSection.classList.remove('invisible');
-    btnNextQuestion.classList.remove('invisible');
+    btnNextQuestion.classList.toggle('invisible');
     startTime();
-    printQuestion();
+    handleNextQuestion();
 }
 
-//
 // Function to go to check question, print next one and restart time
 
 function onNextQuestion(){
     time = 21;
-    checkSelectedAnswer();
-    printQuestion();
+    recalculateScore();
+    handleNextQuestion();
 }
 
-//
 // Function to print questions and answers
 
-function printQuestion(){
+function handleNextQuestion(){
     var btnNextQuestion = document.querySelector('.next__button').disabled = true;
-    let questionsPosition = document.querySelector('.questions__container');
     if (questions.length>0){
-        questionsPosition.innerHTML = (`<h3 class="question__title" id=${questions[0].id}>${questions[0].title}</h3>`);
-        for (const answers of questions[0].answers) {
-            let answerValue = answers.answer;
-            let answerID = answers.id;
-            questionsPosition.innerHTML += 
-                `<label for="${answerID}">${answerValue}</label>
-                <input id="${answerID}" type="radio" value="${answerValue}" name="answerOption" class="input__answer" />`    
-        }
+        printQuestion(questions[0]);
     } 
     if (questions.length == 0) {
         setScores();
     } 
     enableBtnNextQuestion();
     questions = questions.slice(1);
-
 }
-
+function printQuestion(question){
+    var questionsPosition = document.querySelector('.questions__container');
+    questionsPosition.innerHTML = (`<h3 class="question__title" id=${question.id}>${question.title}</h3>`);
+    for (const answers of question.answers) {
+        var answerValue = answers.answer;
+        var answerID = answers.id;
+        questionsPosition.innerHTML += 
+            `<label for="${answerID}">${answerValue}</label>
+            <input id="${answerID}" type="radio" value="${answerValue}" name="answerOption" class="input__answer" />`    
+    }
+}
 //
 //Check if radio buttons are selected to enabled/disabled next question button
+function handleQuestionsTime(on){
+    timeCounter = setInterval(function () {
+        startTime();
+        enableBtnNextQuestion();
+    }, 1000);
+}
 
+
+
+}
 function enableBtnNextQuestion() {
     function enableChecked() {
-        let radios = document.querySelectorAll('.input__answer');
+        var radios = document.querySelectorAll('.input__answer');
         if ((radios[0].checked)||(radios[1].checked)||(radios[2].checked)){
             btnNextQuestion.disabled = false;
         }   
@@ -114,63 +121,6 @@ function enableBtnNextQuestion() {
     }
     setInterval(enableChecked,1000);
 }
-
-// 
-//Check if user selected answer iscorrect
-
-function checkSelectedAnswer(){
-    var radios = document.querySelectorAll('.input__answer');
-    var questionID = document.querySelector('.question__title').id;
-    var currentQuestion = originalQuestions.find(function(originalQuestion){
-        return (questionID == originalQuestion.id);
-    });
-    for (let i = 0; i < radios.length; i++){
-        if (radios[i].checked){
-            let selectedAnswerID = radios[i].id;
-            if(Number(selectedAnswerID) === currentQuestion.correctAnswer){
-                document.querySelector('.answerResult__container').innerHTML = 'Correcto!';
-                recalculateCorrectAnswer();
-            }
-            else{
-                document.querySelector('.answerResult__container').innerHTML = 'Fallaste!';
-                recalculateWrongAnswer();
-            } 
-        }          
-    }
-}
-
-//
-// CALCULATING SCORE
-// Recalcute score if user's answer is correct
-
-function recalculateCorrectAnswer() {
-    if (time <= 18) {
-        score = score + 2;
-    }
-    if (time < 18 && time >= 10) {
-        score = score + 1;
-    }
-    if (time < 10) {
-        score = score;
-    }
-    document.querySelector('.scores__container').innerHTML = score;
-    return score;
-}
-
-// Recalcute score if user's answer is wrong
-
-function recalculateWrongAnswer(){
-    if (time <= 10) {
-        score = score - 1;
-    }
-    if (time > 10) {
-        score = score - 2;
-    }
-    document.querySelector('.scores__container').innerHTML = score;
-    return score;
-}
-
-//
 // Function to keep track of time
 
 function startTime(){
@@ -182,48 +132,99 @@ function startTime(){
         }
         else {
             time = 21;
-            printQuestion();
+            handleNextQuestion();
         }          
     }
-    setInterval(timeControl,1000);
+    (timeControl,1000);
+}
+
+// 
+//Check if user selected answer iscorrect
+
+function recalculateScore(){
+    var radios = document.querySelectorAll('.input__answer');
+    var questionID = document.querySelector('.question__title').id;
+    var currentQuestion = originalQuestions.find(function(originalQuestion){
+        return (questionID == originalQuestion.id);
+    });
+    for (var i = 0; i < radios.length; i++){
+        if (radios[i].checked){
+            var selectedAnswerID = radios[i].id;
+            if(Number(selectedAnswerID) === currentQuestion.correctAnswer){
+                recalculateCorrectAnswer();
+            }
+            else{
+                recalculateWrongAnswer();
+            } 
+        }          
+    }
+}
+
+//
+// CALCULATING SCORE
+// Recalcute score if user's answer is correct
+
+function recalculateCorrectAnswer() {
+    if (time >= 18) {
+        score = score + 2;
+    }
+    if (time < 18 && time >= 10) {
+        score = score + 1;
+    }
+    if (time < 10) {
+        score = score;
+    }
+    document.querySelector('.scores__container').innerHTML = score;
+}
+
+// Recalcute score if user's answer is wrong
+
+function recalculateWrongAnswer(){
+    if (time > 10) {
+        score = score - 1;
+    }
+    if (time <= 10) {
+        score = score - 2;
+    }
+    document.querySelector('.scores__container').innerHTML = score;
 }
 
 //
 
  function setScores(){
-    var btnNextQuestion = document.querySelector('.next__button');
+    var btnNextQuestion = document.querySelector('.playingBoard');
     btnNextQuestion.classList.add('invisible');
-    var questionsSection = document.querySelector('.questionsAnswers');
-    questionsSection.classList.add('invisible');
-    var dataSection = document.querySelector('.data__container');
-    dataSection.classList.add('invisible');
     var userInfo = document.querySelector('.user__info');
     userInfo.classList.remove('invisible');
-
-    var dataSection = document.querySelector('.data__container');
-    dataSection.classList.add('invisible');
-    var user = document.querySelector('.user__info');
-    user.classList.remove('invisible');
-    var dataSection = document.querySelector('.data__container');
-    dataSection.classList.add('invisible');
-    var userName = document.querySelector('.user__name').value;
-    userName.classList.add('invisible');
-    var historicSection = document.querySelector('.historic');
-    historicSection.classList.remove('invisible');
-    if (userName != ''){
-        printUserInfo();
-    }
- }
- function printUserInfo() {
     var btnAddUserName = document.querySelector('.addUser');
-    var usersList = document.querySelector('.usersList');
-    var content = '<li placeholder="Introduce tu nombre">' + userName + ': ' + score + '</li>';
-    usersList.innerHTML += content;
     btnAddUserName.addEventListener("click", printUserInfo);
+ }
+ 
+ function printUserInfo() {
+    var userName = document.querySelector('.user__name').value;
+    if (userName != 0){
+        var usersList = document.querySelector('.usersList');
+        var content = '<li placeholder="Introduce tu nombre">' + userName + ': ' + score + '</li>';
+        usersList.innerHTML += content;
+        var historicSection = document.querySelector('.historic');
+        historicSection.classList.remove('invisible');
+        var btnNewGame = document.querySelector('.startNewGame');
+        
+    }
+    btnNewGame.addEventListener("click", reset);
 
  }
+ function reset(){
+    questions = originalQuestions;
+    score = 0;
+    var historic = document.querySelector('.historic');
+    historic.classList.add('invisible');
+    var userInfo = document.querySelector('.user__info');
+    userInfo.classList.add('invisible');
+    btnStartGame.classList.remove('invisible');
+    onStart();
+ }
 
-btnStartGame.addEventListener("click", onStart);
-btnNextQuestion.addEventListener("click", onNextQuestion);
-btnNewGame.addEventListener("click", onStart);
+ btnStartGame.addEventListener("click", onStart);
+ btnNextQuestion.addEventListener("click", onNextQuestion);
 
